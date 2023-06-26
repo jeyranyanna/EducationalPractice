4d31655239235bfde4479f386b4acdfafc32548d
jeyranyanna <108088695+jeyranyanna@users.noreply.github.com>
2023-06-26 21:12:48 +0500

    Add files via upload

---
+++ Constants.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace HSEPractice2
+{
+    public static class Constants
+    {
+        public static class Roles
+        {
+            public const string Administrator = "Администратор";
+            public const string Director = "Генеральный директор";
+            public const string Cashier = "Кассир";
+            public const string Accountant = "Бухгалтер";
+            public const string Instructor = "Инструктор-спасатель";
+            public const string Animator = "Аниматор";
+            public const string Client = "Клиент";
+        }
+
+        public static class Policies
+        {
+            public const string RequireAdmin = "RequireAdmin";
+            public const string RequireDirector = "RequireDirector";
+            public const string RequireCashier = "RequireCashier";
+            public const string RequireAccountant = "RequireAccountant";
+            public const string RequireInstructor = "RequireInstructor";
+            public const string RequireAnimator = "RequireAnimator";
+            public const string RequireClient = "RequireClient";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using HSEPractice2.Areas.Identity.Data;
+using HSEPractice2;
+using HSEPractice2.Core.Repositories;
+using HSEPractice2.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("WaterparkContextConnection") ?? 
+    throw new InvalidOperationException("Строка подключения 'WaterparkContextConnection' не найдена.");
+
+builder.Services.AddDbContext<WaterparkContext>(options =>
+    options.UseNpgsql(connectionString));
+
+// Проверка почты не обязательна
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<WaterparkContext>();
+
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+#region Авторизация
+
+AddAuthorizationPolicies(builder.Services);
+
+void AddAuthorizationPolicies(IServiceCollection services)
+{
+    services.AddAuthorization(options =>
+    {
+        options.AddPolicy(Constants.Policies.RequireAdmin, policy => policy.RequireRole(Constants.Roles.Administrator));
+        options.AddPolicy(Constants.Policies.RequireDirector, policy => policy.RequireRole(Constants.Roles.Director));
+        options.AddPolicy(Constants.Policies.RequireCashier, policy => policy.RequireRole(Constants.Roles.Cashier));
+        options.AddPolicy(Constants.Policies.RequireAccountant, policy => policy.RequireRole(Constants.Roles.Accountant));
+        options.AddPolicy(Constants.Policies.RequireInstructor, policy => policy.RequireRole(Constants.Roles.Instructor));
+        options.AddPolicy(Constants.Policies.RequireAnimator, policy => policy.RequireRole(Constants.Roles.Animator));
+        options.AddPolicy(Constants.Policies.RequireClient, policy => policy.RequireRole(Constants.Roles.Client));
+    });
+}
+
+#endregion
+
+AddScoped();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();;
+
+app.UseAuthorization();
+
+app.MapRazorPages();// для работы логина и входа
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    endpoints.MapControllerRoute(
+        name: "dashboard",
+        pattern: "{controller=Dashboard}/{action=Index}/{id?}")
+        .RequireAuthorization(); // требуется авторизация для использования маршрута "dashboard"
+});
+
+//app.MapControllerRoute(
+//    name: "default", 
+//    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+app.Run();
+
+void AddScoped()
+{
+    builder.Services.AddScoped<IUserRepository, UserRepository>();
+    builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+    builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+}
